f9588917bbdb43f0cf6d2f93a7d13dce15ce777d
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-02-07 15:25:15 +0200

    install component lib

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,7 +1,13 @@
 import { View } from 'react-native';
+import { Button, Text } from 'react-native-elements'
 
 const RegisterScreen = () => {
-  return <View></View>;
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 26}}>Upload</Text>
+    </View>
+  );
+  
 };
 
 export default RegisterScreen;
