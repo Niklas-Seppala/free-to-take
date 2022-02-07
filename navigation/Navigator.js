ec588e2719bbf6f4c26a9b98a5b151f683f87270
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-02-07 15:19:49 +0200

    implement basic navigation

---
+++ navigation/Navigator.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FontAwesome } from '@expo/vector-icons';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen'
+import UploadScreen from '../screens/UploadScreen'
+import RegisterScreen from '../screens/RegisterScreen'
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const RootNavigator = () => {
+  const loggedIn = true;
+  return (
+    <Stack.Navigator>
+      {loggedIn ? (<Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />) : (
+        <>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+function BottomTabNavigator() {
+  return (
+    <BottomTab.Navigator
+      screenOptions={{ headerShown: true, tabBarActiveTintColor: '#40a62e' }}
+      initialRouteName="Home"
+    >
+      <BottomTab.Screen
+        name="Upload"
+        component={UploadScreen}
+        options={{
+          title: 'Upload',
+          tabBarIcon: ({ color }) => <TabBarIcon name="upload" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+function TabBarIcon({name, color}) {
+  return <FontAwesome size={30} style={{ marginBottom: -3 }} name={name} color={color} />;
+}
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+const RegisterScreen = () => {
+  return <View></View>;
+};
+
+export default RegisterScreen;
