a102a0aca1e43b2be403855092e24a164759b974
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-03-08 15:46:21 +0200

    implement tag selection for upload form

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,6 +1,9 @@
 import { View } from 'react-native';
 import { Button, Text } from 'react-native-elements';
 
+// const [tag, setTag] = useState('');
+{/* <TagSelector onChange={(_, t) => setTag(tag !== t ? t : '')}/>  */}
+
 const RegisterScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
