2de14397b66108c0d35ff67237f8d05e2617d3e4
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-03-09 01:47:01 +0200

    avatar upload done

--- hooks/api/useAvatar.js
+++ hooks/api/useAvatar.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { client, routes } from "../../utils/api";
 import defaultAvatar from '../../assets/user.png'
-
+import { GlobalContext } from "../../context/GlobalContext";
 export default function useAvatar(user) {
   const [avatar, setAvatar] = useState(defaultAvatar);
+  const {apiAction} = useContext(GlobalContext);
+  
   useEffect(async () => {
     const avatarResponse = await client.get(routes.tag.files(`avatar_${user.user_id}`));
     if (avatarResponse.data?.length > 0) {
       setAvatar({uri: routes.uploads.file(avatarResponse.data[0].filename)})
     }
-  }, [user]);
+  }, [user, apiAction]);
 
   return avatar;
 }
