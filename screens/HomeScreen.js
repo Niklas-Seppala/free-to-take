8a46eab7e6ac07ca86d61f7f39462b12c5b480dc
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-03-08 08:30:02 +0200

    pre-merge

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import PostCarousel from '../components/PostCarousel';
+import React, {useEffect, useState} from 'react';
+// import PostCarousel from '../components/PostCarousel';
 import useAllMedia from '../hooks/api/useAllMedia';
-import { ScreenLoader } from '../components/ScreenLoader';
+import {ScreenLoader} from '../components/ScreenLoader';
 import TagFilter from '../components/TagFilter';
-import { useFilters } from '../hooks/useFilters';
-import { useCategories } from "../hooks/useCategories";
+import {useFilters} from '../hooks/useFilters';
+import {useCategories} from '../hooks/useCategories';
+import {FlatList} from 'react-native';
+import { ListItem, Text } from 'react-native-elements';
+
+
+export function ContentListItem({item}) {
+  return (
+    <ListItem>
+      <Text h4>Kakka</Text>
+    </ListItem>
+  );
+}
+
+export function ContentList({data}) {
+  console.log(data);
+  return (
+    <FlatList
+      keyExtractor={(item) => item.file_id.toString()}
+      data={data}
+      renderItem={(item) => <ContentListItem item={item} />}
+    ></FlatList>
+  );
+}
 
 const HomeScreen = () => {
   const [filters, toggle] = useCategories();
@@ -13,16 +35,14 @@ const HomeScreen = () => {
   const filteredData = useFilters(data, filters);
   useEffect(() => setReady(Boolean(data)), [data]);
 
-
   if (!ready) return <ScreenLoader></ScreenLoader>;
 
   // TODO: Test filters when upload is ready.
   return (
     <>
-      <TagFilter
-        onChange={toggle}
-      />
-      <PostCarousel data={data} />
+      <TagFilter onChange={toggle} />
+      <ContentList data={filteredData}></ContentList>
+      {/* <PostCarousel data={data} /> */}
     </>
   );
 };
